0953a81c5d33d969a7cdead4853869467dea590d
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-03-08 15:34:22 +0100

    make mock employee list array

---
+++ src/components/EmployeeList/EmployeeList.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+export default function EmployeeList() {
+
+    const ListaEmpleados = [
+        { 
+            fotoUrl: "https://img.freepik.com/vector-premium/icono-avatar-masculino-persona-desconocida-o-anonima-icono-perfil-avatar-predeterminado-usuario-redes-sociales-hombre-negocios-silueta-perfil-hombre-aislado-sobre-fondo-blanco-ilustracion-vectorial_735449-120.jpg",
+            nombre: "Juan",
+            apellidos: "Perez Cueto",
+            cargo: "Administrador",
+            salario: 2000.00,
+            fecha_ingreso: new Date(2020,8,15),
+            fecha_nacimiento: new Date(1975, 5, 20),
+            telefono: "666123456",
+            email: "juan.perez@gmail.com",
+            localidad: "Barcelona",
+            dni: "12345678Z"
+        },
+        { 
+            fotoUrl: "https://img.freepik.com/vector-premium/icono-avatar-masculino-persona-desconocida-o-anonima-icono-perfil-avatar-predeterminado-usuario-redes-sociales-hombre-negocios-silueta-perfil-hombre-aislado-sobre-fondo-blanco-ilustracion-vectorial_735449-120.jpg",
+            nombre: "Maria",
+            apellidos: "Gonzalez Reyes",
+            cargo: "Gerente de Marketing",
+            salario: 2500.00,
+            fecha_ingreso: new Date(2010,8,15),
+            fecha_nacimiento: new Date(1985, 5, 20),
+            telefono: "666654321",
+            email: "maria.gonzalez@gmail.com",
+            localidad: "Sabadell",
+            dni: "87654321A"
+        },
+        { 
+            fotoUrl: "https://img.freepik.com/vector-premium/icono-avatar-masculino-persona-desconocida-o-anonima-icono-perfil-avatar-predeterminado-usuario-redes-sociales-hombre-negocios-silueta-perfil-hombre-aislado-sobre-fondo-blanco-ilustracion-vectorial_735449-120.jpg",
+            nombre: "Esteban",
+            apellidos: "Dido Loízo",
+            cargo: "CEO",
+            salario: 8000.00,
+            fecha_ingreso: new Date(2015,9,14),
+            fecha_nacimiento: new Date(1978, 3, 11),
+            telefono: "666666666",
+            email: "elvoladordelarbolada@gmail.com",
+            localidad: "Sant Cugat",
+            dni: "12345666X"
+        },
+        { 
+            fotoUrl: "https://img.freepik.com/vector-premium/icono-avatar-masculino-persona-desconocida-o-anonima-icono-perfil-avatar-predeterminado-usuario-redes-sociales-hombre-negocios-silueta-perfil-hombre-aislado-sobre-fondo-blanco-ilustracion-vectorial_735449-120.jpg",
+            nombre: "Miguel",
+            apellidos: "Decer Vantes",
+            cargo: "Editor",
+            salario: 1500.00,
+            fecha_ingreso: new Date(2017,10,1),
+            fecha_nacimiento: new Date(1970, 7, 21),
+            telefono: "632368456",
+            email: "elmancodelepanto@gmail.com",
+            localidad: "Mataró",
+            dni: "12683678Z"
+        },
+    ]
+
+
+  return (
+    <div>
+      funciono
+    </div>
+  )
+}
