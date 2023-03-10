d30ef5147039c412ab8b3dbedb1293f3208cad71
mihaelagheorghiu10 <118915363+mihaelagheorghiu10@users.noreply.github.com>
2023-03-10 09:56:33 +0100

    add style logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 //import { NavbarDiv } from './Navbar.styled'
 import styles from './navbar.module.css'
-
+import logo from '../../assets/Logo.png'
+import { GiHamburgerMenu } from 'react-icons/gi'
 export default function Navbar() {
   return (
     //<NavbarDiv>
     <div className={styles.divContenedor}>
       <div className={styles.divLogo}>
-        <img src="" alt="logoHR"></img>
+        <img className={styles.imgLogo} src={logo} alt="logoHR"></img>
       </div>
       <div className={styles.divTitle}>
         <h1 className={styles.h1Title}>HR App</h1>
       </div>
       <div className={styles.divHamburguesa}>
-        <button className={styles.buttonHamburguesa}></button>
+        <GiHamburgerMenu className={styles.hamburgerIcon} />
       </div>
     </div>
     //</NavbarDiv>
