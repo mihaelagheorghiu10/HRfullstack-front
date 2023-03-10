d01942f521224a478a0b7186177f4f3ea705f08c
jaume vidal <jaumevibu@gmail.com>
2023-03-10 09:10:22 +0100

    Add employeeApiService with all methods to call the api

---
+++ src/apiServices/EmployeeApiService.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:8080/employees";
+
+const employeeApiService = {
+  getAll() {
+    return axios.get(baseUrl).then(response => response.data);
+  },
+  getById(id) {
+    return axios.get(baseUrl + `/${id}`).then((response) => response.data);
+  },
+  searchBy(name, dni, position, location) {
+    return axios.get(
+      baseUrl
+      + `/search?name=${name}&dni=${dni}&position=${position}&location=${location}`)
+      .then(response => response.data);
+  },
+  create(data) {
+    return axios
+      .post(baseUrl, data)
+      .then((response) => response.data)
+      .catch((err) => console.log(err));
+  },
+  editById(id, changes) {
+    return axios
+      .put(baseUrl + `/${id}`, changes)
+      .then((response) => response.data);
+  },
+  deleteById(id) {
+    return axios.delete(baseUrl + `/${id}`).then((response) => response.data);
+  }
+}
+
+export default employeeApiService;
