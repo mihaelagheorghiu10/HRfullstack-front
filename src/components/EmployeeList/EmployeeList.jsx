69d55b14401c6c76816b69eefb18296e905b9f00
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-03-10 11:30:42 +0100

    merge feature/employee branch into dev

--- src/components/EmployeeList/EmployeeList.jsx
+++ src/components/EmployeeList/EmployeeList.jsx
@@ -6,22 +6,20 @@ import employeeApiService from "../../apiServices/EmployeeApiService";
 import { Employee } from "../Employee/Employee";
 
 export default function EmployeeList() {
-    
-    
+  const [employeeList, setEmployeeList] = useState([]);
 
-    const [employeeList, setEmployeeList] = useState([]);
-    
-    useEffect(() => {
-        employeeApiService.getAll().then((data) => {
-          setEmployeeList(data);
-        });
-      }, []);
-    
+  useEffect(() => {
+    employeeApiService.getAll().then((data) => {
+      setEmployeeList(data);
+    });
+  }, []);
+
   return (
-    <div className = {styles.employeeListContainer}>
-      {employeeList.map((employee, index)=> (
+    <div className={styles.employeeListContainer}>
+      {employeeList.map((employee, index) => (
         <Employee employee={employee} key={index} />
       ))}
     </div>
   );
+  
 }
