0cf650b05f77a6bce30905710b0c5fe541456fa2
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-03-15 16:40:44 +0100

    emperimenting with formik form

---
+++ src/Pages/Form/EmployeeForm.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useFormik, Formik, Form, Field, ErrorMessage } from 'formik'
+import styles from './employeeForm.module.css'
+
+export default function EmployeeForm() {
+  
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <form className={styles.formPage} onSubmit={formik.handleSubmit}>
+      <label htmlFor="firstName">First Name</label>
+      <input
+        id="firstName"
+        name="firstName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.firstName}
+      />
+
+      <label htmlFor="lastName">Last Name</label>
+      <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.lastName}
+      />
+
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
