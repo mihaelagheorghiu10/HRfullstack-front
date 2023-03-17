6bd14066f6e79ef004ffe11c3d2ff065f523055f
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-03-17 09:07:50 +0100

    row in-between color change and dividing lines change

--- src/components/EmployeeRow/EmployeeRow.jsx
+++ src/components/EmployeeRow/EmployeeRow.jsx
@@ -7,20 +7,27 @@ export default function EmployeeRow({ employee, deleteById }) {
     <div className={styles.employeeRowContainer}>
       {/* <p className= {styles.employeeId}>{employee.id}</p> */}
       <div className={styles.tableImgContainer}>
-        <img className={styles.tableImg}
+        <img
+          className={styles.tableImg}
           src={employee.photo}
           alt={`Foto ${employee.name} ${employee.lastName}`}
         />
       </div>
-      <p className={styles.employeeName}>{`${employee.name} ${employee.lastName}`}</p>
+      <p
+        className={styles.employeeName}
+      >{`${employee.name} ${employee.lastName}`}</p>
       <p className={styles.employeePosition}>{employee.position}</p>
       <p className={styles.employeePhone}>{employee.phone}</p>
       <p className={styles.employeeEmail}>{employee.email}</p>
       <p className={styles.employeeLocation}>{employee.location}</p>
       <p className={styles.employeeSalary}>{employee.salary}</p>
       <p className={styles.employeeDNI}>{employee.dni}</p>
-      <p className={styles.employeeJoiningDate}>{new Date(employee.joiningDate).toLocaleDateString("es-ES")}</p>
-      <p className={styles.employeeBirthDate}>{new Date(employee.birthDate).toLocaleDateString("es-ES")}</p>
+      <p className={styles.employeeJoiningDate}>
+        {new Date(employee.joiningDate).toLocaleDateString("es-ES")}
+      </p>
+      <p className={styles.employeeBirthDate}>
+        {new Date(employee.birthDate).toLocaleDateString("es-ES")}
+      </p>
       <div className={styles.employeeButtons}>
         <MdEdit className={`${styles.actionButton} ${styles.editButton}`} />
         <MdDeleteForever
