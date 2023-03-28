bcadc396ede381b68cc76d526d9048c425495b6e
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-03-28 18:58:03 +0200

    experimenting with selector

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,19 +1,50 @@
-import React from 'react'
-import styles from './searchBar.module.css'
-import { IoSearch } from 'react-icons/io5'
+import React, { useState } from "react";
+import styles from "./searchBar.module.css";
+import { IoSearch } from "react-icons/io5";
 
 export default function SearchBar() {
+  
+
+  const selectorHandler = (selector) => {
+    switch (selector) {
+      case "Por Localidad": {
+        console.log("se busca por localidad");
+        break;
+      }
+      case "Por Posición": {
+        console.log("se busca por posición");
+        break;
+      }
+      case "Por DNI/NIE": {
+        console.log("se busca por DNI");
+        break;
+      }
+      default: {
+        console.log("se busca por nombre")
+        break;
+      }
+    }
+  }
   return (
     <div className={styles.searchBarContainer}>
-      <select className={styles.searchBarSelector} name="searchBy" id="searchBy">
+      <select
+        className={styles.searchBarSelector}
+        name="searchBy"
+        id="searchBy"
+        onChange={(e)=>(selectorHandler(e.target.value))}
+      >
         <option>Por nombre</option>
         <option>Por Posición</option>
         <option>Por DNI/NIE</option>
         <option>Por Localidad</option>
-
       </select>
-      <input className={styles.searchByNameInput}type="text" placeholder="Buscar" />
-      {/* <IoSearch className={styles.searchIcon}/> */}
+      <input
+        className={styles.searchByNameInput}
+        type="text"
+        placeholder="Buscar"
+        onChange={(e)=>console.log("input: " + e.target.value)}
+      />
+      
     </div>
-  )
+  );
 }
